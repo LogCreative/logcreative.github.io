7e5a8cce591d6b9fb9e5449c9276c206231f4bd4
LogCreative <LogCreative@users.noreply.github.com>
2025-12-11 07:35:31 +0000

    deploy: LogCreative/logcreative.tech@617efcf31ef979cb003e7c82a790d64f6333ff78

--- assets/StarRailCard-web/railcard_config.js
+++ assets/StarRailCard-web/railcard_config.js
@@ -1,2 +1,2 @@
-characters = ["灵砂-5", "长夜月-5", "Saber-5", "遐蝶-5", "卡芙卡-5", "海瑟音-5", "大黑塔-5", "缇宝-5"];
+characters = ["海瑟音-5", "长夜月-5", "缇宝-5", "遐蝶-5", "Saber-5", "卡芙卡-5", "大黑塔-5", "昔涟-5"];
 imgdir = "/assets/StarRailCard-web/RailCard"
--- assets/ZZZCard-web/zzzcard_config.js
+++ assets/ZZZCard-web/zzzcard_config.js
@@ -1,2 +1,3 @@
-characters = ["耀嘉音-5", "仪玄-5", "雅-5", "薇薇安-5", "简-5", "柏妮思-5"];
+characters = ["席德-5", "爱丽丝-5", "卢西娅-5", "薇薇安-5", "耀嘉音-5", "柚叶-5"];
 imgdir = "/assets/ZZZCard-web/ZZZCard"
+// https://act-webstatic.mihoyo.com/game_record/zzzv2/role_square_avatar/role_square_avatar_1401.png?x-oss-process=image/format,webp/quality,Q_90
