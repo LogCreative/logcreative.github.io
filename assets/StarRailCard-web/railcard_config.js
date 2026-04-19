d815f065332b35e14a82e77ee0a9efa7cdbe9cbb
LogCreative <LogCreative@users.noreply.github.com>
2026-04-19 14:43:49 +0000

    deploy: LogCreative/logcreative.tech@9166e88e6652dfbf85d92b1c740245f01bcbccf6

--- assets/StarRailCard-web/railcard_config.js
+++ assets/StarRailCard-web/railcard_config.js
@@ -1,2 +1,2 @@
-characters = ["Saber-5", "遐蝶-5", "昔涟-5", "大黑塔-5", "长夜月-5", "卡芙卡-5", "海瑟音-5", "缇宝-5"];
+characters = ["大黑塔-5", "卡芙卡-5", "遐蝶-5", "缇宝-5", "海瑟音-5", "Saber-5", "长夜月-5", "昔涟-5"];
 imgdir = "/assets/StarRailCard-web/RailCard"
